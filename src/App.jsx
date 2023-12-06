6d62ed306d9848b6088ed02e2553ebc2351789ed
ValkiriaB <valeriabarki@gmail.com>
2023-12-06 20:59:18 -0300

    Agrego Funcionalidades Button,Delete,Task,Filter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
+import React  from 'react';
+import Header from './Components/Header';
 import {Box} from "@chakra-ui/react";
-import { useState } from 'react'
-import  Header from "./Components/Header.jsx"
-import  Form from "./Components/Form.jsx"
-import Button from "./Components/Button.jsx"
-
-function App(){
-
-return(
-  
-<Box bgGradient="linear(red.100 0%, orange.100 25%, yellow.100 50%)"  w="100vw" h="100vh">
-<Header/>
-<Form/>
-<Button/>
-</Box>
-)
+import  FunctionTodo from "./Components/FunctionTodo";
 
 
-
+function App() {
+  return (
+    <Box>
+        <Header/>
+        <FunctionTodo/>
+ </Box> 
+ );
 }
-
-
 
 export default App;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,22 @@
 import { Heading } from "@chakra-ui/react";
-import { Center } from '@chakra-ui/react'
+import { Center,Text } from '@chakra-ui/react'
+import { Flex, IconButton, useColorMode } from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 export default function Header() {
+  const { colorMode, toggleColorMode } = useColorMode()
+
   return (
     <Center h='100px'>
+
     <Heading as='h2' size='xl' >
-      Todo List
+      <Text bgGradient='linear(to-l, #7928CA, #FF0080)'bgClip='text'fontSize='6xl'> To-do List  </Text>
+      <Flex as="header" >
+      <IconButton background="pink"color="white" rounded={50} icon={colorMode === "light" ? <MoonIcon /> : <SunIcon /> }
+      onClick={toggleColorMode}  m={4} position="fixed" top={4}right={4} />
+     </Flex>
     </Heading>
+    
     </Center>
   );
 }
